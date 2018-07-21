01b9471533cd10d1cb48ef2e5004e95fcadcce12
tomjgatenby <40763616+tomjgatenby@users.noreply.github.com>
2018-07-21 14:54:27 +0100

    Added the first test. Used visual studio quick factoring to add Room Class and areDimenionsValid method.

---
+++ BorwellChallengeTests/Features/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BorwellChallengeTests
+{
+    internal class Room
+    {
+        private decimal width;
+        private decimal length;
+        private decimal height;
+
+        public Room(decimal width, decimal length, decimal height)
+        {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+        }
+
+        internal bool areDimensionsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BorwellChallengeTests/Features/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//Test Driven Design for The Room Calculations.
+//Tests Needed:
+//Check Dimensions entered by user are valid.
+//Calculate Area
+//Calculate PaintRequired
+//Calculate Volume
+
+namespace BorwellChallengeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //areDimenstionsValid() method testing.
+        //Test that all of the properties of room are greater than 0.
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal width = 1;
+            decimal length = 1;
+            decimal height = 1;
+            var data = new Room(width, length, height);
+
+            Assert.IsTrue(data.areDimensionsValid());
+        }
+    }
+}
