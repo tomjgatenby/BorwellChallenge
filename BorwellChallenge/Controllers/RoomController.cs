97779fc993a95b835e6f6beda3301e784f7b271f
tomjgatenby <40763616+tomjgatenby@users.noreply.github.com>
2018-07-22 11:17:02 +0100

    Setup RoomController to take parameters and pass a room object to a view. Created simple form in the Main View.

--- BorwellChallenge/Controllers/RoomController.cs
+++ BorwellChallenge/Controllers/RoomController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BorwellChallengeTests.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BorwellChallenge.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Main()
+        public IActionResult Main(decimal width = 0, decimal length = 0,decimal height = 0)
         {
-            return View();
+            Room myRoom = new Room(width, length, height);
+            return View(myRoom);
         }
     }
 }
